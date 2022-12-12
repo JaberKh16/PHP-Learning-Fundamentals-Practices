7d4dedb09d39e686862dd81bdea87bb7b29f756b
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2022-12-12 21:34:00 +0600

    PHP-String-Methods-Concepts

---
+++ 20.String-DataType-Methods/1.Defining-String-Variable-And-Use-Of-Dot(.)-Operator.php
@@ -0,0 +1,29 @@
+/*
+    ----------------------------------------------------------
+                Defining String Type Variables
+    ----------------------------------------------------------
+*/
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Defining String Variables And Use Of Dot Operator</title>
+</head>
+<body>
+    <?php
+        // defining a string
+        $firstName = 'Cristiano';
+        $lastName = 'Ronaldo';
+        $countryName = "Portugal";
+        $age = 38;
+        echo "<br><br>";
+        echo "A Footballer who is "."$firstName"." $lastName". " plays for $countryName and his age is $age.".'<br>';
+    ?>  
+</body>
+</html>
+
+
+
