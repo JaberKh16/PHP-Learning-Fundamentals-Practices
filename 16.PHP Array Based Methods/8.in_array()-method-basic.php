a557ee86835c24ce5b919366b7d942ec731871cd
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-01-21 03:13:12 +0600

    PHP-Array-Based-Methods

--- 16.PHP Array Based Methods/8.in_array()-method-basic.php
+++ 16.PHP Array Based Methods/8.in_array()-method-basic.php
@@ -7,7 +7,15 @@
 
         Syntax:
             in_array(element, $array_name);
-            
+
+
+        Note: in_array() method normally checks for the element in the array with loosly(==) manner, if
+        the wanted strictly type (===) manner way checking then can do like the following:
+
+            1) use the $strict argument which is the third argument of this methods and set it to 'true'
+
+                        in_array(element, $array_name, true);
+
     */
 ?>
 <!DOCTYPE html>
@@ -25,6 +33,48 @@
         var_dump($fruitItems); // printing the array
         echo "<br><br><br>";
         echo "'orange' exists in array: ".in_array('orange',$fruitItems)."<br>"; 
+        // set the check to strict type
+        echo "'Guava' exists in array: ".in_array('Guava',$fruitItems, true)."<br>"; 
+    ?>
+
+    <?php
+        $colors = [
+            'primaryColors' => ['red', 'green', 'blue'],
+            'secondaryColors' => ['cyan', 'magenta', 'yellow', 'black'],
+            'colorVariations' => ['hue', 'saturation', 'lightness']
+        ];
+        
+        if (in_array($colors['primaryColors'] , $colors)) {
+            echo 'Primary Colors found'."<br><br>";
+        } else {
+            echo 'Primary colors are not found'."<br><br>";
+        }
+    ?>
+
+    <?php
+        class Roles{
+            private $id;
+            private $name;
+            public function __construct($id, $name){
+                $this->id = $id;
+                $this->name = $name;
+            }
+        }
+
+        // creating an objects of array
+        $rolesType = [
+            new Roles(1, 'admin'),
+            new Roles(2, 'editor'),
+            new Roles(3, 'user')
+        ];
+
+        
+        if(in_array( new Roles(2, 'editor') , $rolesType)){
+            echo "Roles(2, Editor) existed";
+        }
+        else{
+            echo "Roles(2, Editor) doesn't existed";
+        }
     ?>
 </body>
 </html>
