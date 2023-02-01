7ea63787823ce76be28798b80fc8f6a43f965aec
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-02-01 23:43:30 +0600

    Working-With-Rest-APIs-Using-cURL

--- 25.PHP-With-Rest-APIs/1.cURL-Set-Up-Connection.php
+++ 25.PHP-With-Rest-APIs/1.cURL-Set-Up-Connection.php
@@ -1,12 +1,26 @@
 <?php
-    // create & initialize a curl session 
-    $curl = curl_init();  
-    // set our url with curl_setopt() 
-    curl_setopt($curl, CURLOPT_URL, "api.example.com");  
-    // return the transfer as a string, also with setopt() curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
-    // curl_exec() executes the started curl session and $output contains the output string     
-    $output = curl_exec($curl);  
-    // close curl resource to free up system resources and (deletes the variable made by curl_init) 
-    curl_close($curl);
+
+    function gettingResponse($url, $uri)
+    {
+        // create & initialize a curl session 
+        $curl = curl_init();  
+        // set our url with curl_setopt() 
+        curl_setopt($curl, CURLOPT_URL, $url. $uri);  
+        // return the transfer as a string, also with setopt() curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+        // curl_exec() executes the started curl session and $output contains the output string     
+        $output = curl_exec($curl);
+        
+        // before dumping the data encoded it in json format
+        var_dump(json_encode($output));  
+        
+        // close curl resource to free up system resources and (deletes the variable made by curl_init) 
+        curl_close($curl);
+    }
+
+    // setting the url
+    $url = "https://jsonplaceholder.typicode.com/";
+    $uri = "users";
+
+    gettingResponse($url, $uri);
 
 ?>
