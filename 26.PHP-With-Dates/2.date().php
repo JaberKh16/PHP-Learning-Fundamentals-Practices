938acf0f0702644e495bc287dbe7bde1e8dd4107
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-03-17 01:20:19 +0600

    Working-With-PHP-Date

---
+++ 26.PHP-With-Dates/2.date().php
@@ -0,0 +1,29 @@
+<?php
+
+    // use of date(fmt)
+    $today = date('Y-m-d H:i:s');
+    echo "Today's Date: ". $today;
+
+    echo "<br><br>";
+
+    // get the full day name from the date
+    echo date('l');
+
+
+    echo "<br><br>";
+
+    // get the day name from the date for 3 words
+    echo date('D');
+
+    echo "<br><br>";
+
+    // get the date with leading zero 
+    echo date('j');
+
+
+    echo "<br><br>";
+
+    // use of date with strtotime to get 7days ago info
+    echo date('D-m', strtotime('7days before'));
+
+?>
