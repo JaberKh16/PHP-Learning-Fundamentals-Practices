23b1a19ba20377e95a01d3325218f99e1f5c9ae5
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-03-18 01:04:02 +0600

    Practices-Problems-In-PHP

---
+++ 27.Practicing-Problems-In-PHP/1.Calculating-Distance-Between-Two-Place.php
@@ -0,0 +1,91 @@
+<?php
+    /*
+        To Calculate Distance Betweeen Two Place Follow The Steps
+        =========================================================
+        1) Calculate the distance of longitude of two places
+        2) Convert the longitude value to radians
+        3) Calculate the converted radians consines.
+        4) Convert the latitude of two places to its radians
+        5) Calculate the sine and cosine of both calculated latitudes
+        6) Sine of converted latitudes multiply plus cosine multiplied
+    */
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caluclating The Distance Between Two Places</title>
+</head>
+<body>
+    <?php
+        // gets two places longitude
+        $placeA = [
+            'longitude' => 13.404954,
+            'latitude' => 52.520008
+        ];
+        // gets two places latitude
+        $placeB = [
+            'longitude' => 23.777176,
+            'latitude' => 90.399452
+        ];
+
+
+        function calculateDistanceBetweenTwoPlaces($placeA, $placeB)
+        {
+            // calculating the difference between two places for longitude
+            $longitudeDiffBetweenTwoPlaces = $placeA['longitude'] - $placeB['latitude'];
+            $cosineValueOfLongitude =  calculateTheLongitudeInfo($longitudeDiffBetweenTwoPlaces);   
+
+            // calculating the two places for latitude in Radians
+            $latitudePlaceAValueInRad = deg2rad($placeA['latitude']);
+            $latitudePlaceBValueInRad = deg2rad($placeB['latitude']);
+            $distance = calculateTheLatitudeInfo($latitudePlaceAValueInRad, $latitudePlaceBValueInRad, $cosineValueOfLongitude);
+            echo "Difference between PlaceA and PlaceB is: ". $distance."<br>";
+
+            // calculate the arc distance of the distance
+            $arcCosDistance = acos($distance);
+            // define the radius of earth in km
+            $radiusOfEarthInKM = 6364.934;
+            // calculate the distance in km
+            $distanceInKM = $arcCosDistance * $radiusOfEarthInKM;
+            echo "Distance between PlaceA and PlaceB is: ". $distanceInKM."<br>";
+        }
+
+        function calculateTheLongitudeInfo($longitudeDiffBetweenTwoPlaces)
+        {
+            // converted the longitude value to its radians
+            $longitudeToRadians = deg2rad($longitudeDiffBetweenTwoPlaces);
+            $convertedRadiansConsineForLongitude = cos($longitudeToRadians);
+            return $convertedRadiansConsineForLongitude;
+        }
+
+
+        function calculateTheLatitudeInfo($latitudePlaceAValueInRad, $latitudePlaceBValueInRad, $cosineValueOfLongitude)
+        {
+            // convert the PlaceA and PlaceB latitude radians value to its sin and cosine
+            $latitudePlaceAValueRadInSine = sin($latitudePlaceAValueInRad);
+            $latitudePlaceAValueRadInCosine = cos($latitudePlaceAValueInRad);
+            $latitudePlaceBValueRadInSine = sin($latitudePlaceBValueInRad);
+            $latitudePlaceBValueRadInCosine = cos($latitudePlaceBValueInRad);
+
+            // calcute the latitude of both sin
+            $multiplyOfLatitudeBothPlaceASines = $latitudePlaceAValueRadInSine * $latitudePlaceBValueRadInSine;
+            // calculate the latitude of both cosine
+            $multiplyOfLatitudeBothPlaceBCosines = $latitudePlaceAValueRadInCosine * $latitudePlaceBValueRadInCosine;
+
+            // final formula
+            $formula = $multiplyOfLatitudeBothPlaceASines + $multiplyOfLatitudeBothPlaceBCosines * $cosineValueOfLongitude;
+            return $formula;
+
+        }
+
+
+        calculateDistanceBetweenTwoPlaces($placeA, $placeB);
+
+    ?>
+</body>
+</html>
