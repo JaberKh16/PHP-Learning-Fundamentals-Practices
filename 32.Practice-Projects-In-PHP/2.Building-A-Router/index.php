e887b4cc8f19de241bfd9421bf2fe2b56d9db738
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-04-07 03:52:48 +0600

    PHP-Based-Some-Practical-Projects

--- 32.Practice-Projects-In-PHP/2.Building-A-Router/index.php
+++ 32.Practice-Projects-In-PHP/2.Building-A-Router/index.php
@@ -4,6 +4,11 @@
     // require the router file
     require_once "./router.php";
 
-    Router::handle('GET', '/contact', 'contact.php');
-    echo "In the ".$_SERVER["PHP_SELF"]." file";
+    // Router::handle('GET', '/contact', 'contact.php');
+    // echo "In the ".$_SERVER["PHP_SELF"]." file";
+
+    Router::get('/contact', 'contact.php');
+    Router::get('/contact', function(){
+        echo "Calling from anonymous function"."<br>";
+    });
 ?>
