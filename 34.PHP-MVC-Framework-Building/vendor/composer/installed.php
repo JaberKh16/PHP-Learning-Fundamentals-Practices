eb04f1b0746d4a6f9138f235b505aae082819ff4
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-08-02 04:34:06 +0600

    vendor

---
+++ 34.PHP-MVC-Framework-Building/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jaber/34.php-mvc-framework-building',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3e619e4a11c6fd3127877d2811eeeb30fdcf3d7c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jaber/34.php-mvc-framework-building' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3e619e4a11c6fd3127877d2811eeeb30fdcf3d7c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
