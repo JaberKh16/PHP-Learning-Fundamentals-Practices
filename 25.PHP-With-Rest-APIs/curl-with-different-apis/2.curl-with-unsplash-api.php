5d400d974b0a655606968e99f17f028431ddc53d
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2023-10-06 21:50:43 +0600

    PHP-Rest-API-With-Curl-On-Different-APIs

---
+++ 25.PHP-With-Rest-APIs/curl-with-different-apis/2.curl-with-unsplash-api.php
@@ -0,0 +1,60 @@
+<?php
+
+    function getResponse($url, $access_key)
+    {
+        try{
+            // initiate the curl session
+            $curl = curl_init();
+            $error = curl_error($curl);
+            // set the options for curl session
+            curl_setopt_array(
+                $curl,
+                array(
+                    CURLOPT_URL => $url, // set the url to hit
+                    CURLOPT_TIMEOUT => 1000, // set the timeout to 1sec
+                    CURLOPT_RETURNTRANSFER => true, // set the return of data to store it into variable
+                    CURLOPT_CUSTOMREQUEST => "GET", // set the custom request type
+                    CURLOPT_HTTP_VERSION => "HTTP/1.1", // set the HTTP version
+                    CURLOPT_HTTPHEADER => array(
+                        "Authorization: Client-ID '.$access_key.'",
+                        "Content-Type: application/json",
+                        "Accept: application/json"
+                    ),
+                )
+            );
+
+            // execute the curl session
+            $response = curl_exec($curl);
+            $status_code = curl_getinfo($curl);
+            var_dump($status_code);
+
+
+            // if the status code is not empty
+            if($status_code !=NULL){
+                echo "<pre>";
+                var_dump($response);
+                echo "</pre>";
+            }
+          
+            // close the curl session
+            curl_close($curl);  
+            
+        }
+        catch(Exception $e){
+            print_r($e->getMessage());
+        }
+        finally{
+            $decodedArray = json_decode($response); // returns a StdClass Object 
+            echo "<br><br>";
+            // var_dump($decodedArray);
+           
+        }
+    }
+    // set the url
+    $url = "https://api.unsplash.com/";
+    $access_key = "SFvWrFaJv1Db0Cv5-_elnqvNIwNKEqrxwCELgQOetEE";
+    // calling the function
+    getResponse($url, $access_key);
+
+
+?>
