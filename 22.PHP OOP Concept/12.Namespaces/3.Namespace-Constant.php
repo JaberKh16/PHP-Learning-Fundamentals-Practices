799be15b9c4b11538d4bd7fc53f3ef56f3120033
Jaber Khan <md.jaberkhan66@gmail.com>
2024-06-28 22:34:56 +0600

    PHP-Namespace-Concepts

---
+++ 22.PHP OOP Concept/12.Namespaces/3.Namespace-Constant.php
@@ -0,0 +1,7 @@
+<?php
+
+include "./src/Model/Student.php";
+
+
+echo "Total Marks: " . Student\StudentConstants\TOTAL_MARKS . PHP_EOL;
+echo "Passing Marks: " . Student\StudentConstants\PASSING_MARKS . PHP_EOL;
---
+++ 22.PHP OOP Concept/12.Namespaces/src/Model/Student.php
@@ -0,0 +1,6 @@
+<?php
+    namespace Student\StudentConstants;
+
+    const TOTAL_SUBJECTS = 7;
+    const TOTAL_MARKS = 700;
+    const PASSING_MARKS = 200;
