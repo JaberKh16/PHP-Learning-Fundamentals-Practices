b9500a33dac423718318b060086d0b5c5c2a230c
Jaber Khan <md.jaberkhan66@gmail.com>
2025-07-17 01:22:02 +0600

    PHP-Office-Spreadsheet-Library

--- 26.PHP-With-Dates/2.date().php
+++ 26.PHP-With-Dates/2.date().php
@@ -1,4 +1,25 @@
 <?php
+    /*
+        Date Formation Literal
+        ======================
+        d           --> Day of the month; numeric
+        D           --> Day of the week; short string
+        F           --> Month of the year; long string
+        h           --> Hour; numeric 12-hour format
+        H           --> Hour; numeric 24-hour format
+        i           --> Minute; numeric
+        l           --> Day of the week; long string
+        L           --> Boolean indicating whether it is a leap year
+        m           --> Month of the year; numeric
+        M           --> Month of the year; short string
+        s           --> Seconds; numeric
+        T           --> Timezone
+        Y           --> Year; numeric
+        z           --> Day of the year; numeric
+        
+    */
+?>
+<?php
 
     // use of date(fmt)
     $today = date('Y-m-d H:i:s');
